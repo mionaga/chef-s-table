8d722dac4ba6ff1ce42ffa72c6548a5a3cbce7a7
mionaga <143914849+mionaga@users.noreply.github.com>
2023-11-14 11:54:07 +0000

    タグ機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_13_064601) do
+ActiveRecord::Schema.define(version: 2023_11_14_112634) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -130,6 +130,7 @@ ActiveRecord::Schema.define(version: 2023_11_13_064601) do
     t.integer "cooking_time_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "tag_id"
   end
 
   create_table "steps", force: :cascade do |t|
