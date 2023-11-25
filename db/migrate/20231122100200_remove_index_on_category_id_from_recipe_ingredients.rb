a532b4d3476a2ab7c1b0e0120fc139332cb48201
mionaga <143914849+mionaga@users.noreply.github.com>
2023-11-25 05:09:36 +0000

    migration changed

--- db/migrate/20231122100200_remove_index_on_category_id_from_recipe_ingredients.rb
+++ db/migrate/20231122100200_remove_index_on_category_id_from_recipe_ingredients.rb
@@ -1,5 +1,7 @@
 class RemoveIndexOnCategoryIdFromRecipeIngredients < ActiveRecord::Migration[6.1]
   def change
+    remove_foreign_key :recipe_ingredients, :categories
     remove_index :recipe_ingredients, column: :category_id
+    
   end
 end
