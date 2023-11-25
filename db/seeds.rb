e2591b0f06b4dec68857b6c6366903ea80195e19
mionaga <143914849+mionaga@users.noreply.github.com>
2023-11-25 04:40:37 +0000

    seedファイル変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,35 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Admin.create!(
-   email: 'admin@admin',
-   encrypted_password: 'testtest'
-)
+Admin.find_or_create_by!(email: 'admin@admin') do |admin|
+   admin.password = 'testtest'
+end   
+
 
 5.times do |i|
-  EndUser.find_or_create_by!(
-    family_name: "山田#{i + 1}",
-    first_name: "はな",
-    nickname: "dish#{i+1}",
-    email: "yamada#{i + 1}@yamada",
-    password: "password",
-    specialty: "フランス料理",
-    career_intro: "調理師経歴2年目の新人です",
-    bio: "特技：肉の解体処理"
-  )
+  EndUser.find_or_create_by!(email: "yamada#{i + 1}@yamada") do |end_user|
+    end_user.family_name = "山田#{i + 1}"
+    end_user.first_name = "はな"
+    end_user.nickname = "dish#{i+1}"
+    end_user.password = "password"
+    end_user.specialty = "フランス料理"
+    end_user.career_intro = "調理師経歴2年目の新人です"
+    end_user.bio = "特技：肉の解体処理"
+  end  
 end
 
 5.times do |i|
-  EndUser.find_or_create_by!(
-    family_name: "川田#{i + 1}",
-    first_name: "太郎",
-    nickname: "plate#{i+1}",
-    email: "kawata#{i + 1}@kawata",
-    password: "password",
-    specialty: "イタリア料理",
-    career_intro: "調理師経歴５年目です",
-    bio: "特技：魚捌き"
-  )
+  EndUser.find_or_create_by!(email: "kawata#{i + 1}@kawata") do |end_user|
+    end_user.family_name = "川田#{i + 1}"
+    end_user.first_name = "太郎"
+    end_user.nickname = "plate#{i+1}"
+    end_user.password = "password"
+    end_user.specialty = "イタリア料理"
+    end_user.career_intro = "調理師経歴2年目の新人です"
+    end_user.bio = "特技：肉の解体処理"
+  end  
 end
 
 Tag.create!([
