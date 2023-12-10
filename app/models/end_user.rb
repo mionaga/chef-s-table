e76f3cd7aa76004aaa10916d970c1b353945242a
mionaga <143914849+mionaga@users.noreply.github.com>
2023-12-10 05:13:34 +0000

    end_userモデル修正

--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -16,8 +16,9 @@ class EndUser < ApplicationRecord
     unless profile_image.attached?
       file_path = Rails.root.join("app/assets/images/no_image.jpg")
       profile_image.attach(io: File.open(file_path), filename: "default-image.jpg", content_type: "image/jpeg")
+    else
+      profile_image.variant(resize_to_fill: [width, height], gravity: :center).processed
     end
-    profile_image.variant(resize_to_fill: [width, height], gravity: :center).processed
   end
 
 
